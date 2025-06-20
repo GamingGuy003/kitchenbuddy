e0e6f4539cf87753d3c906c1cbbde39271d89934
bluebird00 <you@example.com>
2025-06-20 17:41:14 +0200

    move shop to context

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Stack } from 'expo-router';
 import { IngredientProvider } from '../context/IngredientContext';
+import { ShopProvider } from '../context/ShopContext';
 
 export default function RootLayout() {
   return (
     <IngredientProvider>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
+      <ShopProvider>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </ShopProvider>
     </IngredientProvider>
   );
 }
